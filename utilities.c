fad26c2d87edd926b693099e8884b12b8f46d396
giulia_neccia <84777485+Giulia-Neccia@users.noreply.github.com>
2021-06-04 18:59:22 +0200

    add print_bitmap+firstIdx

--- utilities.c
+++ utilities.c
@@ -1,7 +1,14 @@
 
-// these are trivial helpers to support you in case you want
-// to do a bitmap implementation
-int levelIdx(size_t idx) { return (int)floor(log2(idx)); };
+#include <stdio.h>
+#include <math.h> // for floor and log2
+#include "buddy_allocator.h"
+#include "bit_map.h"
+
+int levelIdx(size_t idx){
+  if (!idx) return 0;
+  if (idx==1) return 1;
+  return (int)floor(log2(idx+1));
+};
 
 int buddyIdx(int idx) {
   if (idx & 0x1) {
@@ -10,6 +17,28 @@ int buddyIdx(int idx) {
   return idx + 1;
 }
 
-int parentIdx(int idx) { return idx / 2; }
+int parentIdx(int idx) { 
+     return floor((idx-1)/2);
+}
+int startIdx(int idx) {(int idx){
+  return (idx-((1<<levelIdx(idx))-1));
+}
 
-int startIdx(int idx) { return (idx - (1 << levelIdx(idx))); }
+int firstIdx(int lvl){
+  return (1 << lvl) - 1;
+
+}
+
+
+void print_bitmap(BitMap* bit_map){
+  for (int i=0; i<bit_map->num_bits; i++){
+    if (levelIdx(i)!=levelIdx(i+1)){
+      printf("%d ", BitMap_bit(bit_map,i));
+      printf("\n");
+    }
+    else{ 
+     printf("%d ", BitMap_bit(bit_map,i));
+    }
+  }
+  printf("\n");
+  }
