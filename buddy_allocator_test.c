7aeaca2047eb9688c5c40d543882f8799c54a45a
giulia_neccia <84777485+Giulia-Neccia@users.noreply.github.com>
2021-06-06 22:23:15 +0200

    test

--- buddy_allocator_test.c
+++ buddy_allocator_test.c
@@ -1,9 +1,9 @@
 #include "buddy_allocator.h"
 #include <stdio.h>
 
-#define BUFFER_SIZE 102400
-#define BUDDY_LEVELS 9
-#define MEMORY_SIZE (1024*1024)
+#define BUFFER_SIZE 256
+#define BUDDY_LEVELS 3
+#define MEMORY_SIZE 128
 #define MIN_BUCKET_SIZE (MEMORY_SIZE>>(BUDDY_LEVELS))
 
 char buffer[BUFFER_SIZE]; // 100 Kb buffer to handle memory should be enough
@@ -11,12 +11,6 @@ char memory[MEMORY_SIZE];
 
 BuddyAllocator alloc;
 int main(int argc, char** argv) {
-
-  //1 we see if we have enough memory for the buffers
-  int req_size=BuddyAllocator_calcSize(BUDDY_LEVELS);
-  printf("size requested for initialization: %d/BUFFER_SIZE\n", req_size);
-
-  //2 we initialize the allocator
   printf("init... ");
   BuddyAllocator_init(&alloc, BUDDY_LEVELS,
                       buffer,
@@ -26,13 +20,12 @@ int main(int argc, char** argv) {
                       MIN_BUCKET_SIZE);
   printf("DONE\n");
 
-  void* p1=BuddyAllocator_malloc(&alloc, 100);
-  void* p2=BuddyAllocator_malloc(&alloc, 100);
-  void* p3=BuddyAllocator_malloc(&alloc, 100000);
+  void* p1=BuddyAllocator_malloc(&alloc, 27);
+  void* p2=BuddyAllocator_malloc(&alloc, 27);
+  void* p3=BuddyAllocator_malloc(&alloc, 54);
 //   BuddyAllocator_free(&alloc, p1);
 //   BuddyAllocator_free(&alloc, p2);
 //   BuddyAllocator_free(&alloc, p3);
-  void* p5=BuddyAllocator_malloc(&alloc, 100000000);
-  printf("%d\n",p5);
- 
+printf ("%p %p %p", p1,p2,p3);
+return 0; 
 }
